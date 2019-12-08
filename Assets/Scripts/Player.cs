55dec884168e063ca6a1279e6e2cf35ef46895d5
Ryan Churchill <ryan.churchill@gmail.com>
2019-12-08 12:28:58 -0800

    190 animating state in code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,24 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    // properties
+    // constants
+    const string ANIMATOR_PARAM_RUNNING = "Running";
+
+    // config
     [SerializeField] float runSpeed = 5f;
 
-    // cached objects
+    // state
+    bool isAlive = true;
+
+    // cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +37,8 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        myAnimator.SetBool(ANIMATOR_PARAM_RUNNING, isRunning());
     }
 
     private void FlipSprite()
@@ -36,10 +46,15 @@ public class Player : MonoBehaviour
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed)
         {
-            if (Mathf.Sign(myRigidBody.velocity.x) != transform.localScale.x)
+            if (isRunning())
             {
                 transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), transform.localScale.y);
             }
         }
+    }
+
+    private bool isRunning()
+    {
+        return (Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon);
     }
 }
