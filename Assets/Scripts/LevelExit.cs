1da3848be8720987fee4e231c419519907c2a4b5
Ryan Churchill <ryan.churchill@gmail.com>
2020-01-24 18:25:40 -0800

    204 level exit portal

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    Collider2D myCollider;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // next level
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player"))) {
+            StartCoroutine(LoadNextLevelWithDelay());
+        }
+    }
+
+    IEnumerator LoadNextLevelWithDelay()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
