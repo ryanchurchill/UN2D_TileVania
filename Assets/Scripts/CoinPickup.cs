042e49c93dfff662ae45b0d57cd9f2571b03bdb5
Ryan Churchill <ryan.churchill@gmail.com>
2020-01-24 19:07:57 -0800

    207 Coin Pickups

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    Collider2D myCollider;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
